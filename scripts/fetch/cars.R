90d97edacd8058767b399c270a98900ca6ed6f05
Jordan S Read <jread@usgs.gov>
2016-08-12 16:47:45 -0500

    changing names

--- scripts/fetch/cars.R
+++ scripts/fetch/cars.R
@@ -1,4 +1,4 @@
-fetchData.cars <- function(viz) {
+fetch.cars <- function(viz) {
   id <- viz[['id']]
   location <- viz[['location']]
   write.csv(x=cars, file=location, row.names=FALSE)
--- scripts/process/cars.R
+++ scripts/process/cars.R
@@ -1,4 +1,4 @@
-processData.cars <- function(viz) {
+process.cars <- function(viz) {
   data <- readData(viz[['depends']])
   # make loess line (weird example)
   lowess <- lowess(data$speed, data$dist, f = 2/3, iter = 3)
